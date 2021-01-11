ae614b895b6ef0f2c955c129cc550b8895aa80a8
younoch <younoch295@outlook.com>
2021-01-11 23:25:43 +0600

    UI update( Media tab-land: base, background, page1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(()=>{var nameInput,idInput, page1Class, page2Class, page3Class,page4Class, namePass, idpass;
+(()=>{var nameInput,idInput, page1Class, page2Class, page3Class,page4Class, namePass, idpass,page1button;
 // "input warning massage selectors
 nameInput = document.querySelector('#NameWarn');
 idInput = document.querySelector('#idWarn');
@@ -8,6 +8,8 @@ page1Class = document.querySelector('.page1');
 page2Class = document.querySelector('.page2');
 page3Class = document.querySelector('.page3');
 page4Class = document.querySelector('.page4');
+
+page1button = document.querySelector('.page1__submit');
 
 var activePage2 = ()=> {
     var namePass= false, idpass =false;
@@ -27,6 +29,7 @@ var activePage2 = ()=> {
     }else {
         idpass = true;
         idInput.style.visibility = 'hidden';
+
     }
     if(namePass && idpass) {
         page1Class.style.visibility = 'hidden';
@@ -34,6 +37,8 @@ var activePage2 = ()=> {
         document.querySelector('.page2__user-name').innerHTML = username;
         nameInput.style.visibility = 'hidden';
         idInput.style.visibility = 'hidden';
+    } else {
+        page1button.style.animation = 'floting2 .1s 3';
     }
 }
 if(activePage2.username!== '')
